d61becce54a27675742dcd47ef3dbb3e3d45b85b
paabloomaartinez <pablo121002@gmail.com>
2023-01-27 20:31:39 +0100

    Agregar archivos de proyecto.

---
+++ CalculoIVA_DLL/CalculoIVA.vb
@@ -0,0 +1,15 @@
+﻿Public Class CalculoIVA
+
+    Private cantidad As Double
+    Public Function totalSinIVA(ByVal cantidad As Double, ByVal tipo As Double) As Double
+
+        totalSinIVA = (cantidad / tipo)
+
+    End Function
+
+    Public Function IVACorrespondiente(ByVal cantidad As Double) As Double
+
+        IVACorrespondiente = 
+
+    End Function
+End Class
