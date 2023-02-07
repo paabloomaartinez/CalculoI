8ab84a03dc60b33ce280f5931d16b0b8bb722cea
paabloomaartinez <pablo121002@gmail.com>
2023-02-07 11:34:27 +0100

    Empezando el punto 3

---
+++ CapaDeDatos/GeneradorDeDatos.vb
@@ -0,0 +1,19 @@
+﻿Public Class GeneradorDeDatos
+
+    Private Shared ImportesFacturasYTiposIVA As Double(,)
+
+    Private Sub New()
+        ImportesFacturasYTiposIVA = New Double(,) {{2500, 10}, {3150.55, 21}, {25000, 21}, {315.35, 4}}
+    End Sub
+
+    Public Shared Function totalDeLaFactura(ByVal indice As Integer) As Double
+        Dim c As New GeneradorDeDatos()
+        totalDeLaFactura = ImportesFacturasYTiposIVA.GetValue(indice)
+    End Function
+
+    Public Shared Function tipoIVADeLaFactura(ByVal indice As Integer) As Double
+        Dim c As New GeneradorDeDatos()
+        tipoIVADeLaFactura = ImportesFacturasYTiposIVA.GetValue(indice)
+    End Function
+
+End Class
